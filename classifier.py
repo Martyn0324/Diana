06d0385d764dc78f096612a340c4943eadd9281a
Martyn <giovanni.ab@outlook.com>
2022-06-22 18:01:18 -0300

    We're gonna need images
    So no Wiscosin Dataset for us

--- classifier.py
+++ classifier.py
@@ -12,55 +12,7 @@ import torch.optim as optim
 import torch.utils.data
 import torchvision.utils as vutils
 from torchsummary import summary
-
-data = DatasetCreator.images(PATH, 100, 100)
-
-data = data.astype('float32') # Necessary. Otherwise the normalization will return only black squares
-
-#data = data/127.5 - 1.0 # Dangerous operation --> Might collapse CPU
-
-for i in range(len(data)):
-    data[i] = data[i]/127.5 - 1.0 # This one probably does the same, but in a smoother way. It won't collapse your CPU...I guess...
-
-
-X_train = data[:2000]
-
-classes = ("Normal", "Threat")
-
-'''for i in range(0, 2000): # Plotting the images so we can label them
-    X_train[i] = (X_train[i]+1.0)*0.5 # Denormalizing
-    plt.imshow(X_train[i])
-    plt.title(f"image {i}")
-    plt.show()'''
-
-y_train = [1, 1, 0, 0, 2, 3, 2, 1] # Continue on as you wish.
-
-y_train = np.array(y_train)
-y_train = to_categorical(y_train, 5)
-
-print(y_train[0])
-print(y_train.shape)
-
-print(len(X_train), len(y_train))
-
-X_test = data[2000:2050]
-
-for i in range(X_test.shape[0]):
-    X_test[i] = (X_test[i]+1.0)*0.5
-    plt.imshow(X_test[i])
-    plt.title(f'image {i}')
-    plt.show()
-    
-y_test = [0, 0, 1, 0, 2, 0, 0, 2, 0, 1, 0, 0, 1, 1, 2,
-        1, 0, 2, 1, 0, 1, 1, 0, 2, 1, 0, 1, 1, 1, 0,
-        1, 1, 0, 0, 1, 1, 0, 0, 0, 2, 1, 0, 0, 0, 1,
-        1, 1, 1, 1, 0]
-
-y_test = np.array(y_test)
-y_test = to_categorical(y_test, 4)
-
-print(y_test[0])
-print(y_test.shape)
+from dataset import X_train, X_test, y_train, y_test
 
 
 '''model = Sequential()
